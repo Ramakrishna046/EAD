96cdf1072cfb21d9f36485cc4b06125eb2f36e29
Ramakrishna046 <163405658+Ramakrishna046@users.noreply.github.com>
2025-10-26 19:51:49 +0530

    Delete JWT authorization directory

--- JWT authorization/controllers/user.js
+++
@@ -1,3 +0,0 @@
-exports.getUserProfile = (req, res) => {
-  res.json({ message: "Welcome to your profile", user: req.user });
-};
--- JWT authorization/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUserProfile } = require("../controllers/user");
-const { verifyToken } = require("../middleware/auth");
-
-const router = express.Router();
-
-router.get("/profile", verifyToken, getUserProfile);
-
-module.exports = router;
